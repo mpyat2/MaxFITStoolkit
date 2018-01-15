47c2652cef0c134d644a0af3a856448547f2da2a
mmpyat <mmpyat@5b3607db-4df2-0244-8a27-3f6f91efc260>
2018-01-15 09:38:28 +0000

    git-svn-id: https://max-pc/svn/MaxSVNrepository/Astro/trunk/Utils@2066 5b3607db-4df2-0244-8a27-3f6f91efc260

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 rem SET COMPILER="C:\PERSONAL\DC\DCC32" -UUnits\ -UFreeImage\ -Ebin-out\
-SET COMPILER="c:\lazarus\fpc\3.0.2\bin\i386-win32\fpc.exe" -MDELPHI -Xg -FuUnits\ -FuFreeImage\ -FEbin-out\
+SET COMPILER="c:\personal\lazarus\fpc\3.0.2\bin\i386-win32\fpc.exe" -MDELPHI -Xg -FuUnits\ -FuFreeImage\ -FEbin-out\
 SET A7z="c:\Program Files\7-zip\7z.exe"
 
 REM fihed.exe: Print/edit FITS header, multifile mode
@@ -55,12 +55,16 @@ REM ...
 IF ERRORLEVEL 1 GOTO :ERROR
 ECHO .
 
+IF "%1"=="A" GOTO :ARCHIVE
+GOTO :END
+:ARCHIVE
 del FITSutils_src.zip
 %A7z% a FITSutils_src.zip FitsHeader2\fihed.* FitsFlip\fflip.* IrisDateObs\idobs.* IrisRename\iren.* IPDAT\ipdat.* IPDAT\testdata\*.dat APDAT\apdat.* APDAT\testdata\*.dat FitsCfa\fitscfa.* FitsRGB\fitsrgb.* iconvraw\iconvraw.* iconvraw\FreeImage.dll Hello\*.pas Units\*.pas FreeImage\* Setup\*.iss Setup\output\dirinfo bin-out\dirinfo bin-out\fitsutils.ini Samples\* bin-out\fitsutils.txt clean.bat clean2.bat make.bat
 
-GOTO :EOF
+GOTO :END
 
 :ERROR
 ECHO ***** ERROR! CANNOT COMPILE.
 
+:END
 pause
