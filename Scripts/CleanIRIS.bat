0771d0ea3715ca27346fc512a557c37dc57678e3
max <max@5b3607db-4df2-0244-8a27-3f6f91efc260>
2018-07-29 12:34:59 +0000

    git-svn-id: https://max-pc/svn/MaxSVNrepository/Astro/trunk/Utils@2217 5b3607db-4df2-0244-8a27-3f6f91efc260

--- Scripts/CleanIRIS.bat
+++ Scripts/CleanIRIS.bat
@@ -1,1 +1,5 @@
-@del *.pol *.lst "@*.fit" "#*.fit"
+@echo off
+for %%i in ("*.pol")  do del %%i
+for %%i in ("@*.fit") do del %%i
+for %%i in ("#*.fit") do del %%i
+for %%i in (*.lst)    do if /i not [%%i]==[_cosme.lst] del %%i
