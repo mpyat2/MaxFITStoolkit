51350a885419455ec41a2beae580f61f46fc8fd1
mmpyat <mmpyat@5b3607db-4df2-0244-8a27-3f6f91efc260>
2018-07-30 14:46:35 +0000

    Intermediate commit
    git-svn-id: https://max-pc/svn/MaxSVNrepository/Astro/trunk/Utils@2218 5b3607db-4df2-0244-8a27-3f6f91efc260

---
+++ Scripts/MakeMasters.bat
@@ -0,0 +1,42 @@
+@ECHO OFF
+IF [%1]==[] GOTO :NOPARAM
+
+REM master-offset
+MakeStack /f /a /m=m /o=. /g=_offset                           /format=i16 offset*.fit
+IF ERRORLEVEL 1 GOTO :ERROR
+
+REM master-dark
+MakeStack /f /a /m=m /o=. /g=_dark   /sub=_offset              /format=i16 dark*.fit
+IF ERRORLEVEL 1 GOTO :ERROR
+
+REM master-flat
+REM fix /norm= value in case of overflow
+MakeStack /f /a /m=m /o=. /g=_flat   /sub=_offset /norm=10000  /format=i16 flat*.fit
+IF ERRORLEVEL 1 GOTO :ERROR
+
+REM hot-pixel-map
+find_hot _dark.fit /n=_cosme /l=%1
+IF ERRORLEVEL 1 GOTO :ERROR
+
+GOTO :EOF
+
+:NOPARAM
+ECHO MakeMasters creates masters
+ECHO   _offset.fit
+ECHO   _dark.fit
+ECHO   _flat.fit
+ECHO from sets of individual 
+ECHO   offset*.fit
+ECHO   dark*.fit
+ECHO   flat*.fit
+ECHO files.
+ECHO It also makes IRIS-compatible hot-pixel-map _cosme.lst.
+ECHO The only parameter you should specify is a "hot-pixel threshold".
+ECHO This parameter will be used while generating hot-pixel-map _cosme.lst.
+ECHO You can always regenerate hot-pixel-map (using arbitrary threshold)
+ECHO with "FindHot" command (assuming _dark.fit exists).
+ECHO Usage:
+ECHO MakeMasters {threshold}
+GOTO :EOF
+:ERROR
+ECHO ***** ERROR!!!
---
+++ Scripts/MakePRscript.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+IF [%1]==[] GOTO :NOPARAM
+SET SCRIPTNAME=preprocess.pgm
+ECHO load _dark                                               > %SCRIPTNAME%
+ECHO add _offset                                             >> %SCRIPTNAME%
+ECHO save __dark_with_offset                                 >> %SCRIPTNAME%
+ECHO load _flat                                              >> %SCRIPTNAME%
+ECHO grey_flat                                               >> %SCRIPTNAME%
+ECHO save __flat_neutral                                     >> %SCRIPTNAME%
+ECHO pr light __dark_with_offset __flat_neutral ###c###_ %1  >> %SCRIPTNAME%
+ECHO cosme_cfa2 ###c###_ c_light _cosme %1                   >> %SCRIPTNAME%
+GOTO :EOF
+:NOPARAM
+ECHO MakePRscript creates IRIS "preprocess.pgm" script for a specified number
+ECHO of light images.
+ECHO To use the resulting script from within IRIS you should prepare
+ECHO master-frames by "MakeMasters" command.
+ECHO A sequence of yourlight images should have generic name "light"
+ECHO with numbering from 1 to N_of_images, i.e.:
+ECHO   light1.fit
+ECHO   light2.fit
+ECHO   [...]
+ECHO Resulting sequence of calibrated images will be c_light*.fit
+ECHO Usage:
+ECHO MakePRscript {N_of_images}
