3af8cd853dda51ae9c3717f2a4e9d3b5587e2637
mpyat2 <mpyat2@gmail.com>
2025-07-08 09:15:19 +0300

    dcdft was removed (now it is a part of the LCV project)

--- dcdft/test_dcdft.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-DEL eta_Aql_VisualData_2024_result.txt
-dcdft.exe -t -l=0.1 -h=0.6 -n=10000 eta_Aql_VisualData_2024.tsv eta_Aql_VisualData_2024-FP-result.txt
-IF ERRORLEVEL 1 GOTO :ERROR
-notepad eta_Aql_VisualData_2024-FP-result.txt
-GOTO :END
-:ERROR
-ECHO *** ERROR
-:END
