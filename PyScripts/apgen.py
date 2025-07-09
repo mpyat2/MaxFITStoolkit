49d82dd2ba16ca4dcf1438bc2165a26ed996cf7a
mpyat2 <44919945+mpyat2@users.noreply.github.com>
2025-07-09 13:50:55 +0300

    Update apgen.py
    Added --plot functionality

--- PyScripts/apgen.py
+++ PyScripts/apgen.py
@@ -1,36 +1,65 @@
+"""
+
+Title: Aperture list generator (for AstroImageJ)
+Description: see below.
+Author: Maksym Pyatnytskyy
+Email: mpyat2@gmail.com
+Date: 2025-07-09
+Version: 1.1
+
+License: MIT License
+
+Usage: apgen.py [-h] [--fwhm FWHM] [--plot] filename [output]
+
+Required packages: astropy, photutils, matplotlib, numpy, argparse, sys
+
+"""
+
+DESCRIPTION = \
+"""The script takes an input FITS image (which must contain WCS) and extracts
+the coordinates of the star-like sources into an output file."""
+
 from astropy.io import fits
 from astropy.wcs import WCS
-from photutils.detection import DAOStarFinder
 from astropy.stats import sigma_clipped_stats
 from astropy.coordinates import Angle
 import astropy.units as u
+from photutils.detection import DAOStarFinder
+import matplotlib.pyplot as plt
+import numpy as np
 import argparse
 import sys
-#import numpy as np
-
-#RADEC_LIST = 'radec_list.txt'
 
 def deg_to_sexagesimal(ra_deg, dec_deg):
     ra = Angle(ra_deg, unit=u.deg).to_string(unit=u.hour, sep=':', precision=2, pad=True)
     dec = Angle(dec_deg, unit=u.deg).to_string(unit=u.deg, sep=':', precision=1, alwayssign=True, pad=True)
     return ra, dec
 
-def process_image(file_name, output_list, fwhm):
+def process_image(file_name, output_list, fwhm, plot_image):
     # Load FITS image
     hdu = fits.open(file_name)[0]
-    data = hdu.data
+    header = hdu.header
+    if 'OBJECT' in header:
+        object_name = header['OBJECT']
+    else:
+        object_name = '<Unknown>'
+    image_data = hdu.data
     wcs = WCS(hdu.header)
     
     # Estimate background and noise
-    mean, median, std = sigma_clipped_stats(data, sigma=3.0)
+    mean, median, std = sigma_clipped_stats(image_data, sigma=3.0)
     threshold = median + (5. * std)
+    print()
     print('Background Median    =', median)
     print('Background StDev     =', std)
     print('Detection Threshold  =', threshold)
     
     # Detect stars
     daofind = DAOStarFinder(fwhm=fwhm, threshold=threshold)
-    sources = daofind(data - median)
+    sources = daofind(image_data - median)
+    
+    print()
+    print(str(len(sources)) + ' sources found')
     
     # Convert pixel to RA/Dec
     world_coords = wcs.pixel_to_world(sources['xcentroid'], sources['ycentroid'])
@@ -38,9 +67,31 @@ def process_image(file_name, output_list, fwhm):
     # Write to RA/Dec file
     with open(output_list, "w") as f:
         for ra, dec in zip(world_coords.ra.deg, world_coords.dec.deg):
+#            star_coords.append((ra, dec))
             ra_str, dec_str = deg_to_sexagesimal(ra, dec)
             f.write(f"{ra_str}, {dec_str}, 0, 1, 99.999\n")
+    print()
     print('List of objects coordinates has been created:', output_list)
+    
+    if plot_image:
+        vmin = np.percentile(image_data, 1)  # 1st percentile
+        vmax = np.percentile(image_data, 99)  # 99th percentile
+        fig = plt.figure(figsize=(6, 6))
+        ax = fig.add_subplot(111, projection=wcs)
+        im = ax.imshow(image_data, cmap='gray', origin='lower', vmin=vmin, vmax=vmax)
+        ax.coords.grid(color='white', ls='dotted')
+        ax.coords[0].set_axislabel('Right Ascension')
+        ax.coords[1].set_axislabel('Declination')
+        plt.colorbar(im, ax=ax, orientation='vertical')
+        plt.title('Object: ' + str(object_name))
+
+        # Mark the stars with small circles
+        for i in range(len(sources)):
+            x = sources['xcentroid'][i]
+            y = sources['ycentroid'][i]
+            circle = plt.Circle((x, y), radius=fwhm / 2.0, color='red', fill=False)
+            plt.gca().add_patch(circle)
+        plt.show()
 
 def restricted_float(x):
     try:
@@ -52,15 +103,18 @@ def restricted_float(x):
     return x
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Process an image file with a specified FWHM.")
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
     # Required positional argument: input file
     parser.add_argument("filename", type=str, help="Path to the input image file")
     # Optional positional argument: output file with default
     parser.add_argument("output", nargs="?", default="radec_list.txt",
-                        help="Output file name (default: radec_list.txt)")
+                        help="Output file name. Default: radec_list.txt")
     # Optional named argument: FWHM
-    parser.add_argument("--fwhm", type=restricted_float, default=3.0,
-                        help="Full Width at Half Maximum (FWHM) in pixels (1–20). Default: 3.0")
+    parser.add_argument("--fwhm", type=restricted_float, default=4.0,
+                        help="Full Width at Half Maximum (FWHM) in pixels (1–20). Default: 4.0")
+    # Optional boolean argument: show plot
+    parser.add_argument('--plot', action='store_true', default=False,
+                        help='Show the image with apertures. Default: do not show)')
     return parser.parse_args()
 
 def main():
@@ -68,7 +122,8 @@ def main():
     #print(f"Input file: {args.filename}")
     #print(f"Output file: {args.output}")
     #print(f"FWHM: {args.fwhm}")
-    process_image(args.filename, args.output, args.fwhm)
+    #print(f"Plot: {args.plot}")
+    process_image(args.filename, args.output, args.fwhm, args.plot)
 
 if __name__ == "__main__":
     try:
@@ -77,5 +132,5 @@ if __name__ == "__main__":
         print(f"Fatal Error: {e}. Press ENTER to continue:")
         input("")
     finally:
-        print("End")
+        #print("End")
         sys.exit()
